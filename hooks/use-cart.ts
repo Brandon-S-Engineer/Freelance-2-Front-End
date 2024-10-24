d5b1f1842616bbe45e205b26ff22a693c2e98752
Brandon <fortuneblue25@gmail.com>
2024-10-24 12:28:57 -0600

    u

--- hooks/use-cart.ts
+++ hooks/use-cart.ts
@@ -4,20 +4,12 @@ import { Product } from '@/types';
 
 interface CartStore {
   items: Product[];
+  addItem: (data: Product) => void;
+  removeItem: (id: string) => void;
+  removeAll: () => void;
 }
 
 // Zustand store to manage the preview modal state
-const useCart = create<CartStore>((set) => ({
-  isOpen: false,
-
-  // `data` Will hold the product data when the modal is opened.
-  data: undefined,
-
-  // `onOpen` is a function that takes product data and sets the modal to open with that data.
-  onOpen: (data: Product) => set({ data, isOpen: true }),
-
-  // `onClose` is a function that sets the modal to closed without any data.
-  onClose: () => set({ isOpen: false }),
-}));
+const useCart = create<CartStore>((set) => ({}));
 
 export default useCart;
