503b5618ffc3f9dab25a7de76c6a553bed61987d
Brandon <fortuneblue25@gmail.com>
2024-12-03 17:10:01 -0600

    u

--- components/info.tsx
+++ components/info.tsx
@@ -6,11 +6,22 @@ import { Product } from '@/types';
 import Currency from '@/components/ui/currency';
 import Button from '@/components/ui/button';
 
+import { MouseEventHandler } from 'react';
+import useCart from '@/hooks/use-cart';
+
 interface InfoProps {
   data: Product;
 }
 
 const Info: React.FC<InfoProps> = ({ data }) => {
+  const cart = useCart();
+
+  const onAddToCart: MouseEventHandler<HTMLButtonElement> = (event) => {
+    event.stopPropagation();
+
+    cart.addItem(data);
+  };
+
   return (
     <div>
       <h1 className='text-3xl font-bold text-gray-900'>{data.name}</h1>
@@ -42,7 +53,9 @@ const Info: React.FC<InfoProps> = ({ data }) => {
       </div>
 
       <div className='mt-10 flex items-center gap-x-3'>
-        <Button className='flex items-center gap-x-2 text-white'>
+        <Button
+          onClick={onAddToCart}
+          className='flex items-center gap-x-2 text-white'>
           Add To Cart <ShoppingCart />
         </Button>
       </div>
